0dc72f7e21bd7c6d294eb52e9bf3ce2ae6868061
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-03-08 11:24:13 +1100

    Added priority to flash messages

--- tests/unit/services/flash-messages-service-test.js
+++ tests/unit/services/flash-messages-service-test.js
@@ -20,6 +20,31 @@ QUnit.module('FlashMessagesService', {
   }
 });
 
+test('#queue returns an array of flash messages', function(assert) {
+  assert.expect(1);
+
+  run(function() {
+    service.success('success 1');
+    service.success('success 2');
+    service.success('success 3');
+  });
+
+  assert.equal(service.get('queue.length'), 3);
+});
+
+test('#arrangedQueue returns an array of flash messages, sorted by priority', function(assert) {
+  assert.expect(2);
+
+  run(function() {
+    service.success('success 1', { priority: 100 });
+    service.success('success 2', { priority: 200 });
+    service.success('success 3', { priority: 300 });
+  });
+
+  assert.equal(service.get('arrangedQueue.length'), 3);
+  assert.equal(service.get('arrangedQueue.0.priority'), 300);
+});
+
 test('#success adds a success message', function(assert) {
   assert.expect(3);
 
@@ -60,7 +85,22 @@ test('#addMessage adds a custom message', function(assert) {
   assert.expect(3);
 
   run(function() {
-    SANDBOX.flash = service.addMessage('custom', 'test');
+    SANDBOX.flash = service.addMessage('custom', { type: 'test' });
+  });
+
+  assert.equal(service.get('queue.length'), 1);
+  assert.equal(service.get('queue.0'), SANDBOX.flash);
+  assert.equal(service.get('queue.0.type'), 'test');
+});
+
+test('#add adds a custom message', function(assert) {
+  assert.expect(3);
+
+  run(function() {
+    SANDBOX.flash = service.add({
+      message: 'Test message please ignore',
+      type: 'test'
+    });
   });
 
   assert.equal(service.get('queue.length'), 1);
