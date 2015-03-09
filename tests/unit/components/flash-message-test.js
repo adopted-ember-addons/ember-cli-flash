23b244dfb509ec6edc39421a6c74f97fc1a9fff3
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-03-09 14:45:16 +1100

    ES6ified all the things - fixed formatting - reordererd classes - updated readme - added test for priority - added integration test - updated unit tests - es6ified all the things - added babel preprocessor to the addon

--- tests/unit/components/flash-message-test.js
+++ tests/unit/components/flash-message-test.js
@@ -2,20 +2,53 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import FlashMessage from 'ember-cli-flash/flash/object';
+
+var flash;
 
 moduleForComponent('flash-message', 'FlashMessageComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  beforeEach() {
+    flash = FlashMessage.create({
+      message  : 'test',
+      type     : 'test',
+      timeout  : 500,
+      priority : 500
+    });
+  },
+
+  afterEach() {
+    flash = null;
+  }
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
-  assert.equal(component.state, 'preRender');
+  const component = this.subject();
+  assert.equal(component._state, 'preRender');
 
   // render the component on the page
   this.render();
-  assert.equal(component.state, 'inDOM');
+  assert.equal(component._state, 'inDOM');
+});
+
+test('#alertType returns the right type', function(assert) {
+  assert.expect(1);
+
+  const component = this.subject();
+  component.set('flash', flash);
+
+  this.render();
+  assert.equal(component.get('alertType'), 'alert-test');
+});
+
+test('#flashType returns the right classified alert type', function(assert) {
+  assert.expect(1);
+
+  const component = this.subject();
+  component.set('flash', flash);
+
+  this.render();
+  assert.equal(component.get('flashType'), 'Test');
 });
