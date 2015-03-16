f05ae89585bc04b94b190a80b34ae8f85016ac22
leojpod <leo.jpod@gmail.com>
2015-03-16 19:20:20 +0000

    adding some not working test...

--- tests/acceptance/integration-test.js
+++ tests/acceptance/integration-test.js
@@ -52,3 +52,42 @@ test('sticky messages are not removed automatically', function(assert) {
     assert.equal(find('.alert.alert-danger p').text(), 'You went offline');
   });
 });
+test('sticky messages are still removed when clicked', function(assert) {
+  assert.expect(5);
+  
+  visit('/');
+
+  andThen(() => {
+    assert.ok(find('.alert.alert-danger'));
+    assert.equal(find('.alert').length, 1);
+    assert.equal(find('.alert.alert-danger h6').text(), 'Danger');
+    assert.equal(find('.alert.alert-danger p').text(), 'You went offline');
+  });
+  
+  // click('.alert.alert-danger');
+  triggerEvent('.alert.alert-danger', $(document), 'click');
+  andThen(() => {
+    assert.equal(find('.alert').length, 0);
+  });
+});
+
+test('mouseover-ed messages are not removed automatically', function (assert) {
+  assert.expect(8);
+  visit('/');
+  triggerEvent('.alert.alert-success', 'mouseenter');
+  
+  assert.ok(find('.alert.alert-success'), 'right after the rendering the flash message is there!');
+  andThen(() => {
+    assert.ok(find('.alert.alert-success'), 'the success message received the mouseenter and stayed on display');
+    assert.equal(find('.alert').length, 2); //there should be the sticky and the mouseover-ed
+    assert.equal(find('.alert.alert-success h6').text(), 'Success');
+    assert.equal(find('.alert.alert-success p').text(), 'Route transitioned successfully');
+  });
+  triggerEvent('.alert.alert-success', 'mouseleave');
+  andThen(() => {
+    assert.equal(find('.alert').length, 1);//only the sicky one should be there now...
+    assert.ok(find('.alert.alert-danger'));
+    assert.equal(find('.alert.alert-danger h6').text(), 'Danger');
+    assert.equal(find('.alert.alert-danger p').text(), 'You went offline');
+  });
+});
