18b79a3493cb6c8a5759f29cc6ee50969d89e886
bl4ckm0r3 <marco.sparagna@gmail.com>
2017-07-28 10:24:08 -0700

    Upgraded to ember 2.14.1 slightly changed tests to use wait

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "globals": {
+        "$": true
+    }
+};
