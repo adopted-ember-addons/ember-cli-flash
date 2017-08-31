9880d129301e82f94653e0f11b635e37ab25b1b6
cmcclure <cmcclure@simplymeasured.com>
2017-08-31 13:20:30 -0700

    Add timeout example to dummy app

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -8,7 +8,15 @@ export default Ember.Route.extend({
 
     flashMessages.success('Route transitioned successfully', {
       priority: 500,
-      showProgress: true
+      showProgress: true,
+    });
+
+    flashMessages.success('Three second timout with a two second exit', {
+      priority: 100,
+      showProgress: true,
+      sticky: false,
+      timeout: 3000,
+      extendedTimeout: 2000
     });
 
     flashMessages.warning('It is going to rain tomorrow', {
