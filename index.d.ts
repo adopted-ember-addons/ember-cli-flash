e797af5103b773136cd7c5b09193611dcc8ee15a
Mike North <michael.l.north@gmail.com>
2018-01-06 13:08:26 -0800

    TypeScript type information (#258)

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+declare module 'ember-cli-flash/flash/object' {
+  
+  import EmberObject from '@ember/object';
+  import Evented from '@ember/object/evented';
+  
+  class FlashObject extends EmberObject.extend(Evented) {
+    exiting: boolean;
+    exitTimer: number;
+    isExitable: boolean;
+    initializedTime: number;
+    destroyMessage(): void;
+    exitMessage(): void;
+    preventExit(): void;
+    allowExit(): void;
+    timerTask(): void;
+    exitTimerTask(): void;
+  }
+  export default FlashObject;
+}
+
+declare module 'ember-cli-flash/services/flash-messages' {
+
+  import Service from '@ember/service';
+  import FlashObject from 'ember-cli-flash/flash/object';
+
+  type Partial<T> = { [K in keyof T]?: T[K] };
+
+  interface MessageOptions {
+    type: string;
+    priority: number;
+    timeout: number;
+    sticky: boolean;
+    showProgress: boolean;
+    extendedTimeout: number;
+    destroyOnClick: boolean;
+    onDestroy: () => void;
+  }
+  
+  interface CustomMessageInfo extends Partial<MessageOptions> {
+    message: string;
+  }
+  
+  interface FlashFunction {
+    (message: string, options?: Partial<MessageOptions>): FlashMessageService;
+  }
+  
+  class FlashMessageService extends Service {
+    success: FlashFunction;
+    warning: FlashFunction;
+    info: FlashFunction;
+    danger: FlashFunction;
+    alert: FlashFunction;
+    secondary: FlashFunction;
+    add(messageInfo: CustomMessageInfo): FlashMessageService;
+    clearMessages(): FlashMessageService;
+    registerTypes(types: string[]): FlashMessageService;
+    getFlashObject(): FlashObject;
+  }
+
+  export default FlashMessageService;
+}
