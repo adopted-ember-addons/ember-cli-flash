51571a67193cfd45a34ac3e3be4d55349f58a1d0
Scott Batson <sjbats@email.com>
2018-10-12 17:54:39 -0400

    add qunit-dom and update component tests

--- tests/integration/components/flash-message-test.js
+++ tests/integration/components/flash-message-test.js
@@ -1,144 +1,138 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
 import FlashMessage from 'ember-cli-flash/flash/object';
 import wait from 'ember-test-helpers/wait';
+import { click, render, triggerEvent } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
 
 const timeoutDefault = 1000;
 
-moduleForComponent('flash-message', 'Integration | Component | flash message', {
-  integration: true
-});
+module('Integration | Component | flash message', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders a flash message', function(assert) {
-  this.set('flash', FlashMessage.create({ message: 'hi', sticky: true }));
-
-  this.render(hbs`
-    {{#flash-message flash=flash as |component flash|}}
-      {{flash.message}}
-    {{/flash-message}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'hi');
-  assert.ok(this.$('.alert:eq(0)').hasClass('flash-message'));
-});
-
-test('it does not error when quickly removed from the DOM', function(assert) {
-  this.set('flash', FlashMessage.create({ message: 'hi', sticky: true }));
-  this.set('flag', true);
-
-  this.render(hbs`
-    {{#if flag}}
+  test('it renders a flash message', async function(assert) {
+    this.set('flash', FlashMessage.create({ message: 'hi', sticky: true }));
+  
+    await render(hbs`
       {{#flash-message flash=flash as |component flash|}}
         {{flash.message}}
       {{/flash-message}}
-    {{/if}}
-  `);
-
-  this.set('flag', false);
-
-  return wait().then(() => {
-    assert.ok(this.get('flash').isDestroyed, 'Flash Object isDestroyed');
+    `);
+  
+    assert.equal(this.element.textContent.trim(), 'hi');
+    assert.dom('.alert').hasClass('flash-message');
   });
-});
-
-test('flash message is removed after timeout', function(assert) {
-  assert.expect(3);
-
-  this.set('flash', FlashMessage.create({
-    message: 'hi',
-    sticky: false,
-    timeout: timeoutDefault
-  }));
-
-  this.render(hbs`
-    {{#flash-message flash=flash as |component flash|}}
-      {{flash.message}}
-    {{/flash-message}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'hi');
-  assert.notOk(this.get('flash').isDestroyed, 'Flash is not destroyed immediately');
-
-  return wait().then(() => {
+  
+  test('it does not error when quickly removed from the DOM', async function(assert) {
+    this.set('flash', FlashMessage.create({ message: 'hi', sticky: true }));
+    this.set('flag', true);
+  
+    await render(hbs`
+      {{#if flag}}
+        {{#flash-message flash=flash as |component flash|}}
+          {{flash.message}}
+        {{/flash-message}}
+      {{/if}}
+    `);
+  
+    this.set('flag', false);
+  
+    return wait().then(() => {
+      assert.ok(this.get('flash').isDestroyed, 'Flash Object isDestroyed');
+    });
+  });
+  
+  test('flash message is removed after timeout', async function(assert) {
+    assert.expect(3);
+  
+    this.set('flash', FlashMessage.create({
+      message: 'hi',
+      sticky: false,
+      timeout: timeoutDefault
+    }));
+  
+    render(hbs`
+      {{#flash-message flash=flash as |component flash|}}
+        {{flash.message}}
+      {{/flash-message}}
+    `);
+    assert.notOk(this.get('flash').isDestroyed, 'Flash is not destroyed immediately');
+    await wait();
+    assert.equal(this.element.textContent.trim(), 'hi');
+  
+    await wait();
     assert.ok(this.get('flash').isDestroyed, 'Flash Object is destroyed');
   });
-});
+  
+  test('flash message is not removed after timeout if mouse enters', async function(assert) {
+    assert.expect(2);
+  
+    let flashObject = FlashMessage.create({
+      message: 'hi',
+      sticky: false,
+      timeout: timeoutDefault
+    });
+  
+    this.set('flash', flashObject);
+  
+    render(hbs`
+      {{#flash-message elementId="testFlash" flash=flash as |component flash|}}
+        {{flash.message}}
+      {{/flash-message}}
+    `);
 
-test('flash message is removed after timeout if mouse enters', function(assert) {
-  assert.expect(3);
-
-  let flashObject = FlashMessage.create({
-    message: 'hi',
-    sticky: false,
-    timeout: timeoutDefault
-  });
-
-  this.set('flash', flashObject);
-
-  this.render(hbs`
-    {{#flash-message elementId="testFlash" flash=flash as |component flash|}}
-      {{flash.message}}
-    {{/flash-message}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'hi');
-  this.$('#testFlash').mouseenter();
-
-  assert.notOk(flashObject.isDestroyed, 'Flash Object is not destroyed');
-  this.$('#testFlash').mouseleave();
-
-  return wait().then(() => {
+    await triggerEvent('#testFlash', 'mouseover');
+    await wait();
+    assert.notOk(flashObject.isDestroyed, 'Flash Object is not 44destroyed');
+    await triggerEvent('#testFlash', 'mouseout');
+  
+    await wait();
     assert.ok(flashObject.isDestroyed, 'Flash Object is destroyed');
   });
-});
-
-test('a custom component can use the close closure action', function(assert) {
-  assert.expect(3);
-
-  this.set('flash', FlashMessage.create({
-    message: 'flash message content',
-    sticky: true,
-    destroyOnClick: false
-  }));
-
-  this.render(hbs`
-    {{#flash-message flash=flash as |component flash close|}}
-      {{flash.message}}
-      <a href="#" {{action close}}>close</a>
-    {{/flash-message}}
-  `);
-
-  assert.notOk(this.get('flash').isDestroyed, 'flash has not been destroyed yet');
-  this.$(":contains(flash message content)").click();
-  assert.notOk(this.get('flash').isDestroyed, 'flash has not been destroyed yet');
-  this.$(":contains(close)").click();
-  return wait().then(() => {
+  
+  test('a custom component can use the close closure action', async function(assert) {
+    assert.expect(3);
+  
+    this.set('flash', FlashMessage.create({
+      message: 'flash message content',
+      sticky: true,
+      destroyOnClick: false
+    }));
+  
+    await render(hbs`
+      {{#flash-message flash=flash as |component flash close|}}
+        {{flash.message}}
+        <a role="close" href="#" {{action close}}>close</a>
+      {{/flash-message}}
+    `);
+  
+    assert.notOk(this.get('flash').isDestroyed, 'flash has not been destroyed yet');
+    await click('.alert');
+    assert.notOk(this.get('flash').isDestroyed, 'flash has not been destroyed yet');
+    await click('[role="close"]');
     assert.ok(this.get('flash').isDestroyed, 'flash is destroyed after clicking close');
   });
-});
-
-test('exiting class is applied for sticky messages', function(assert) {
-  assert.expect(3);
-  let flashObject =  FlashMessage.create({
-    message: 'flash message content',
-    sticky: true,
-    extendedTimeout: 100
-  });
-
-  this.set('flash', flashObject);
-
-  this.render(hbs`
-    {{#flash-message flash=flash as |component flash|}}
-      <span>{{flash.message}}</span>
-    {{/flash-message}}
-  `);
-
-  const flashDiv = this.$('.alert:eq(0)');
-  flashDiv.click();
-  assert.ok(flashDiv.length, 'Flash message is shown');
-  assert.ok(flashDiv.hasClass('exiting'), 'exiting class is applied');
-
-  return wait().then(() => {
+  
+  test('exiting class is applied for sticky messages', async function(assert) {
+    assert.expect(3);
+    let flashObject =  FlashMessage.create({
+      message: 'flash message content',
+      sticky: true,
+      extendedTimeout: 100
+    });
+  
+    this.set('flash', flashObject);
+  
+    await render(hbs`
+      {{#flash-message flash=flash as |component flash|}}
+        <span>{{flash.message}}</span>
+      {{/flash-message}}
+    `);
+  
+    await click('.alert');
+    assert.dom('.alert').exists();
+    assert.dom('.alert').hasClass('exiting');
+  
     assert.ok(flashObject.isDestroyed, 'Flash Object is destroyed');
   });
 });
--- tests/unit/components/flash-message-test.js
+++ tests/unit/components/flash-message-test.js
@@ -1,19 +1,16 @@
 import { run } from '@ember/runloop';
 import { set, get } from '@ember/object';
-import $ from 'jquery';
-import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
 import FlashMessage from 'ember-cli-flash/flash/object';
 import wait from 'ember-test-helpers/wait';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
 let flash;
 
-moduleForComponent('flash-message', 'FlashMessageComponent', {
-  unit: true,
+module('Unit | Component | flash-message', function (hooks) {
+  setupTest(hooks);
 
-  beforeEach() {
+  hooks.beforeEach(function() {
     flash = FlashMessage.create({
       message: 'test',
       type: 'test',
@@ -21,57 +18,61 @@ moduleForComponent('flash-message', 'FlashMessageComponent', {
       extendedTimeout: 5000,
       showProgress: true
     });
-  },
-
-  afterEach() {
-    flash = null;
-  }
-});
-
-test('it renders with the right props', async function(assert) {
-  assert.expect(5);
-
-  const component = this.subject({ flash });
-  assert.equal(get(component, 'active'), false, 'it initializes with `active` set to false');
-
-  this.render();
-  assert.equal(get(component, 'alertType'), 'alert alert-test', 'it has the right `alertType`');
-  assert.equal(get(component, 'flashType'), 'Test', 'it has the right `flashType`');
-  assert.equal(get(component, 'progressDuration'), `transition-duration: ${flash.get('timeout')}ms`, 'it has the right `progressDuration`');
-
-  await wait();
-  assert.equal(get(component, 'active'), true, 'it sets `active` to true after rendering');
-});
-
-test('exiting the flash object sets exiting on the component', function(assert) {
-  assert.expect(2);
-
-  const component = this.subject({ flash });
-  this.render();
-  assert.equal(get(component, 'exiting'), false, 'it initializes with `exiting` set to false');
-
-  run(() => {
-    set(flash, 'exiting' , true);
-    assert.ok(get(component, 'exiting'), 'it sets `exiting` to true when the flash object is exiting');
   });
-});
 
-test('it destroys the flash object on click', function(assert) {
-  assert.expect(1);
-  flash.set('extendedTimeout', 0);
-  const component = this.subject({ flash });
-  this.render();
-
-  $('.alert').click();
-  assert.ok(get(component, 'flash').isDestroyed, 'it destroys the flash object on click');
-});
-
-test('it does not destroy the flash object when `flash.destroyOnClick` is false', function(assert) {
-  assert.expect(1);
-  flash.destroyOnClick = false;
-  const component = this.subject({ flash });
-  this.render();
-
-  $('.alert').click();
-  assert.notOk(get(component, 'flash').isDestroyed, 'it does not destroy the flash object on click');
+  hooks.afterEach(function() {
+    flash = null;
+  });
+  
+  test('it renders with the right props', async function(assert) {
+    assert.expect(5);
+  
+    let component = this.owner.factoryFor('component:flash-message').create();
+    component.set('flash', flash);
+    assert.equal(get(component, 'active'), false, 'it initializes with `active` set to false');
+  
+    component.didInsertElement();
+    assert.equal(get(component, 'alertType'), 'alert alert-test', 'it has the right `alertType`');
+    assert.equal(get(component, 'flashType'), 'Test', 'it has the right `flashType`');
+    assert.equal(get(component, 'progressDuration'), `transition-duration: ${flash.get('timeout')}ms`, 'it has the right `progressDuration`');
+  
+    await wait();
+    assert.equal(get(component, 'active'), true, 'it sets `active` to true after rendering');
+  });
+  
+  test('exiting the flash object sets exiting on the component', function(assert) {
+    assert.expect(2);
+  
+    let component = this.owner.factoryFor('component:flash-message').create();
+    component.set('flash', flash);
+    component.didInsertElement();
+    assert.equal(get(component, 'exiting'), false, 'it initializes with `exiting` set to false');
+  
+    run(() => {
+      set(flash, 'exiting' , true);
+      assert.ok(get(component, 'exiting'), 'it sets `exiting` to true when the flash object is exiting');
+    });
+  });
+  
+  test('it destroys the flash object on click', function(assert) {
+    assert.expect(1);
+    flash.set('extendedTimeout', 0);
+    let component = this.owner.factoryFor('component:flash-message').create();
+    component.set('flash', flash);
+    component.didInsertElement();
+  
+    run(() => component.click());
+    assert.ok(get(component, 'flash').isDestroyed, 'it destroys the flash object on click');
+  });
+  
+  test('it does not destroy the flash object when `flash.destroyOnClick` is false', function(assert) {
+    assert.expect(1);
+    flash.destroyOnClick = false;
+    let component = this.owner.factoryFor('component:flash-message').create();
+    component.set('flash', flash);
+    component.didInsertElement();
+  
+    run(() => component.click());
+    assert.notOk(get(component, 'flash').isDestroyed, 'it does not destroy the flash object on click');
+  });
 });
