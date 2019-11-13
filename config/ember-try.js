1e8c49be90c5c430ef00e8b94f9806e0542f30f1
Abhilash <abhilashlr.789@gmail.com>
2019-11-13 09:24:44 +0530

    Update node 8
    Test node version
    
    Marking it allowed since release is not yet supported
    
    Adding 3.4 and 3.8 into ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -47,7 +47,24 @@ module.exports = function() {
           }
         },
         {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0'
+            }
+          }
+        },
+        {
           name: 'ember-release',
+          allowedToFail: true,
           npm: {
             devDependencies: {
               'ember-source': urls[0]
