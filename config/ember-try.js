104a016765f001cf22e9a60a719b068d0937a01d
Abhilash <abhilashlr.789@gmail.com>
2019-11-13 22:54:08 +0530

    Test jquery addition

--- config/ember-try.js
+++ config/ember-try.js
@@ -26,15 +26,17 @@ module.exports = function() {
           name: 'ember-lts-2.12',
           npm: {
             devDependencies: {
-              'ember-source': '~2.12.0'
+              'ember-source': '~2.12.0',
+              '@ember/jquery': '^0.5.1'
             }
-          }
+          },
         },
         {
           name: 'ember-lts-2.16',
           npm: {
             devDependencies: {
-              'ember-source': '~2.16.0'
+              'ember-source': '~2.16.0',
+              '@ember/jquery': '^0.5.1'
             }
           }
         },
@@ -42,7 +44,8 @@ module.exports = function() {
           name: 'ember-lts-2.18',
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0'
+              'ember-source': '~2.18.0',
+              '@ember/jquery': '^0.5.1'
             }
           }
         },
