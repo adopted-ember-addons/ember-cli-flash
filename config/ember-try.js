5252fc21bfab891977d2093bbf0764d9d21f0e6e
abhilashlr <abhilashlr.789@gmail.com>
2019-11-14 19:51:51 +0530

    CHORE: Update node version on CircleCI to 8 (#309)
    * Update node 8
    
    Test node version
    
    Marking it allowed since release is not yet supported
    
    Adding 3.4 and 3.8 into ember-try
    
    * Removes flag for ember-release

--- config/ember-try.js
+++ config/ember-try.js
@@ -63,6 +63,22 @@ module.exports = function() {
           }
         },
         {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0'
+            }
+          }
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
