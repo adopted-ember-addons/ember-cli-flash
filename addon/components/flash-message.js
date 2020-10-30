fe229546ccbd6998a93d559eb40782c37061c4d9
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-30 15:55:54 +0100

    Remove unnecessary `ember-decorators` dependency (#334)

--- addon/components/flash-message.js
+++ addon/components/flash-message.js
@@ -4,13 +4,12 @@ import { isPresent } from '@ember/utils';
 import { run } from '@ember/runloop';
 import { action, computed, set } from '@ember/object';
 import { and, bool, readOnly, not } from '@ember/object/computed';
-import { tagName } from '@ember-decorators/component';
 import layout from '../templates/components/flash-message';
 
 const { next, cancel } = run;
 
-@tagName('')
 export default class FlashMessage extends Component {
+  tagName = '';
   layout = layout;
   active = false;
   messageStyle = 'bootstrap';
