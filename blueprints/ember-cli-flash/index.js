97e7f28ba8433f7760d5cceb28e239a61d67ed05
Peter Wagenet <peter.wagenet@gmail.com>
2021-02-26 09:10:39 -0800

    Remove old Ember support from blueprints - Fixes #340, #347 (#351)
    The check was breaking with ember-cli-version-checker v5. However,
    we don't need to generate blueprint for versions of Ember this old
    anyway.

--- blueprints/ember-cli-flash/index.js
+++ blueprints/ember-cli-flash/index.js
@@ -1,5 +1,4 @@
 var EOL = require('os').EOL;
-let VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   description: 'Generates ember-cli-flash test helper',
@@ -7,20 +6,9 @@ module.exports = {
   afterInstall: function() {
     var TEST_HELPER_PATH = 'tests/test-helper.js';
     var IMPORT_STATEMENT = EOL + "import './helpers/flash-message';";
-    var INSERT_AFTER_PRE_2_17 = "import resolver from './helpers/resolver';";
-    var INSERT_AFTER_POST_2_17 = "import { start } from 'ember-qunit';";
+    var INSERT_AFTER = "import { start } from 'ember-qunit';";
 
-    let checker = new VersionChecker(this);
-    let ember = checker.forEmber();
-
-    let after;
-    if (ember.isAbove('2.17.0')) {
-      after = INSERT_AFTER_POST_2_17;
-    } else {
-      after = INSERT_AFTER_PRE_2_17;
-    }
-
-    return this.insertIntoFile(TEST_HELPER_PATH, IMPORT_STATEMENT, { after });
+    return this.insertIntoFile(TEST_HELPER_PATH, IMPORT_STATEMENT, { after: INSERT_AFTER });
   },
 
   normalizeEntityName: function() {}
