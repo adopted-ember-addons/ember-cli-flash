c7a78ada9d9becada10cc4a5bc780efa82019a7b
Travis Hoover <thoov7@gmail.com>
2021-05-19 11:37:49 -0700

    Adding embroider/test-setup to ember-try scenarios
    Adds two new scenarios for ember-try: embroider-safe and embroider-optimized.
    Embroider-safe does not enable any tree shaking optimizations where as
    embroider-optimized enables them all.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = function () {
   return Promise.all([
@@ -76,6 +77,8 @@ module.exports = function () {
             },
           },
         },
+        embroiderSafe(),
+        embroiderOptimized(),
       ],
     };
   });
