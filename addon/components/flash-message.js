0f4c0ac2acc2c3397686f82bbaa63959a6faa1c4
Charles Fries <charliefries@icloud.com>
2022-06-16 10:04:42 -0700

    Convert `flash-message` to Glimmer component (#382)
    * Convert `flash-message` to Glimmer component
    
    * Fix computed dependant key
    
    * Move Glimmer packages to dependencies
    
    * ember-template-lint: Disable `no-invalid-interactive` for `flash-message` component

--- addon/components/flash-message.js
+++ addon/components/flash-message.js
@@ -1,29 +1,50 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { classify } from '@ember/string';
 import { htmlSafe } from '@ember/template';
 import { isPresent } from '@ember/utils';
 import { next, cancel } from '@ember/runloop';
-import { action, computed, set } from '@ember/object';
-import { and, readOnly, not } from '@ember/object/computed';
-import layout from '../templates/components/flash-message';
+import { action, computed } from '@ember/object';
+
+/**
+ * ARGS
+ *
+ * flash: FlashObject
+ * messageStyle?: 'bootstrap' | 'foundation'
+ * messageStylePrefix?: string
+ */
 
 export default class FlashMessage extends Component {
-  tagName = '';
-  layout = layout;
-  active = false;
-  messageStyle = 'bootstrap';
+  @tracked active = false;
 
-  @readOnly('flash.showProgress')
-  showProgress;
+  @tracked pendingSet;
+  @tracked _mouseEnterHandler;
+  @tracked _mouseLeaveHandler;
 
-  @not('exiting')
-  notExiting;
+  get messageStyle() {
+    return this.args.messageStyle ?? 'bootstrap';
+  }
 
-  @and('showProgress', 'notExiting')
-  showProgressBar;
+  get showProgress() {
+    return this.args.flash.showProgress;
+  }
 
-  @readOnly('flash.exiting')
-  exiting;
+  get notExiting() {
+    return !this.exiting;
+  }
+
+  get showProgressBar() {
+    return this.showProgress && this.notExiting;
+  }
+
+  @computed('args.flash.exiting')
+  get exiting() {
+    return this.args.flash.exiting;
+  }
+
+  get messageStylePrefix() {
+    return this.args.messageStylePrefix ?? this._defaultMessageStylePrefix;
+  }
 
   @computed('messageStyle')
   get _defaultMessageStylePrefix() {
@@ -31,48 +52,50 @@ export default class FlashMessage extends Component {
     return isFoundation ? 'alert-box ' : 'alert alert-';
   }
 
-  @computed('flash.type', 'messageStylePrefix', '_defaultMessageStylePrefix')
+  @computed('args.flash.type', 'messageStylePrefix')
   get alertType() {
-    const flashType = this.flash.type || '';
-    const prefix = this.messageStylePrefix || this._defaultMessageStylePrefix;
+    const flashType = this.args.flash.type || '';
+    const prefix = this.messageStylePrefix;
     return `${prefix}${flashType}`;
   }
 
-  @computed('flash.type')
+  @computed('args.flash.type')
   get flashType() {
-    return classify(this.flash.type || '');
+    return classify(this.args.flash.type || '');
   }
 
-  @computed('flash.{showProgress,timeout}')
+  @computed('args.flash.{showProgress,timeout}')
   get progressDuration() {
-    if (!this.flash?.showProgress) {
+    if (!this.args.flash?.showProgress) {
       return false;
     }
-    const duration = this.flash?.timeout || 0;
+    const duration = this.args.flash?.timeout || 0;
     return htmlSafe(`transition-duration: ${duration}ms`);
   }
 
+  @action
   _mouseEnter() {
-    if (isPresent(this.flash)) {
-      this.flash.preventExit();
+    if (isPresent(this.args.flash)) {
+      this.args.flash.preventExit();
     }
   }
 
+  @action
   _mouseLeave() {
-    if (isPresent(this.flash) && !this.flash.exiting) {
-      this.flash.allowExit();
+    if (isPresent(this.args.flash) && !this.args.flash.exiting) {
+      this.args.flash.allowExit();
     }
   }
 
   _destroyFlashMessage() {
-    if (this.flash) {
-      this.flash.destroyMessage();
+    if (this.args.flash) {
+      this.args.flash.destroyMessage();
     }
   }
 
   @action
   onClick() {
-    const destroyOnClick = this.flash?.destroyOnClick ?? true;
+    const destroyOnClick = this.args.flash?.destroyOnClick ?? true;
 
     if (destroyOnClick) {
       this._destroyFlashMessage();
@@ -87,11 +110,11 @@ export default class FlashMessage extends Component {
   @action
   onDidInsert(element) {
     const pendingSet = next(this, () => {
-      set(this, 'active', true);
+      this.active = true;
     });
-    set(this, 'pendingSet', pendingSet);
-    set(this, '_mouseEnterHandler', this._mouseEnter);
-    set(this, '_mouseLeaveHandler', this._mouseLeave);
+    this.pendingSet = pendingSet;
+    this._mouseEnterHandler = this._mouseEnter;
+    this._mouseLeaveHandler = this._mouseLeave;
     element.addEventListener('mouseenter', this._mouseEnterHandler);
     element.addEventListener('mouseleave', this._mouseLeaveHandler);
   }
