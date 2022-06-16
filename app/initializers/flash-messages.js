2c7d3f5882c96f1e30111ab0cfd0e73625c8dc19
Robert Wagner <rwwagner90@gmail.com>
2022-06-16 10:03:11 -0400

    Update flash-messages.js

--- app/initializers/flash-messages.js
+++ app/initializers/flash-messages.js
@@ -1,4 +1,4 @@
-import config from 'ember-cli-flash/config/environment';
+import config from '../config/environment';
 import { deprecate } from '@ember/debug';
 
 /* eslint-disable ember/new-module-imports */
