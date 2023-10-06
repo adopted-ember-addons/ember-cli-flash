57eb1d2cc89a754fd786042c25e0814f1ea17392
Giles Thompson <giles@papaiti.com>
2023-10-07 12:55:24 +1300

    chore(test-app): ember-cli-update from v4.4.0...v4.12.2

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
     },
   },
   plugins: ['ember'],
@@ -29,6 +32,7 @@ module.exports = {
       files: [
         './.eslintrc.js',
         './.prettierrc.js',
+        './.stylelintrc.js',
         './.template-lintrc.js',
         './ember-cli-build.js',
         './index.js',
@@ -44,11 +48,7 @@ module.exports = {
         browser: false,
         node: true,
       },
-      rules: {
-        'node/no-unsupported-features/es-syntax': 'warn',
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
+      extends: ['plugin:n/recommended'],
     },
     {
       // test files
--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,31 +1,14 @@
 'use strict';
 
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberAddon(defaults, {
-    babel: {
-      plugins: [
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-      ],
-      loose: false,
+  let app = new EmberApp(defaults, {
+    autoImport: {
+      watchDependencies: ['blueprinted'],
     },
   });
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
-  });
+  return maybeEmbroider(app);
 };
