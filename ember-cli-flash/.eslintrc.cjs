2724d8433a6aa93bb11d40e9d0abaa7fecb96003
Giles Thompson <giles@papaiti.com>
2023-10-07 12:18:41 +1300

    chore(v2 conversion): setup addon package from @embroider/addon-blueprint

---
+++ ember-cli-flash/.eslintrc.cjs
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    babelOptions: {
+      root: __dirname,
+    },
+  },
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        './.eslintrc.cjs',
+        './.prettierrc.cjs',
+        './.template-lintrc.cjs',
+        './addon-main.cjs',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['n'],
+      extends: ['plugin:n/recommended'],
+    },
+  ],
+};
