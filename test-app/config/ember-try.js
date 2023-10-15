1c12a7398a5dc94c2bfea76e71337566f8a272c9
Giles Thompson <giles@papaiti.com>
2023-10-15 14:33:19 +1300

    chore(ci): drop embroider scenarios

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
@@ -65,8 +64,6 @@ module.exports = async function () {
           }),
         },
       },
-      embroiderSafe(),
-      embroiderOptimized(),
     ],
   };
 };
