9b7df5524e090d6c94e8d03ced5ad98621014c87
Giles Thompson <giles@papaiti.com>
2023-10-24 17:00:11 +1300

    refactor(service): drop compouted use

--- ember-cli-flash/src/services/flash-messages.js
+++ ember-cli-flash/src/services/flash-messages.js
@@ -1,8 +1,7 @@
-/* eslint-disable ember/no-computed-properties-in-native-classes */
 import Service from '@ember/service';
 import { typeOf, isNone } from '@ember/utils';
 import { warn, assert } from '@ember/debug';
-import { set, setProperties, computed } from '@ember/object';
+import { set, setProperties } from '@ember/object';
 import { classify } from '@ember/string';
 import FlashMessage from '../flash/object';
 import objectWithout from '../utils/object-without';
@@ -92,8 +91,10 @@ export default class FlashMessagesService extends Service {
     );
 
     const flashService = this;
-    const allDefaults = this.flashMessageDefaults ?? {};
-    const defaults = objectWithout(allDefaults, ['types', 'preventDuplicates']);
+    const defaults = objectWithout(this.flashMessageDefaults, [
+      'types',
+      'preventDuplicates',
+    ]);
 
     const flashMessageOptions = Object.assign({}, defaults, { flashService });
 
@@ -110,7 +111,7 @@ export default class FlashMessagesService extends Service {
   }
 
   _getOptionOrDefault(key, value) {
-    const defaults = this.flashMessageDefaults ?? {};
+    const defaults = this.flashMessageDefaults;
     const defaultOption = defaults[key];
 
     if (typeOf(value) === 'undefined') {
@@ -120,7 +121,6 @@ export default class FlashMessagesService extends Service {
     return value;
   }
 
-  @computed
   get flashMessageDefaults() {
     const config = getOwner(this).resolveRegistration('config:environment');
     const overrides = config.flashMessageDefaults ?? {};
@@ -128,7 +128,7 @@ export default class FlashMessagesService extends Service {
   }
 
   _setDefaults() {
-    const defaults = this.flashMessageDefaults ?? {};
+    const defaults = this.flashMessageDefaults;
 
     for (let key in defaults) {
       const classifiedKey = classify(key);
